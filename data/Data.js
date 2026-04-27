aa072e63cededacb4413552578c3292bebd0c7cc
Shruti <designshruti.0104@gmail.com>
2026-04-27 23:38:52 +0530

    Create Data.js

---
+++ data/Data.js
@@ -0,0 +1,38 @@
+const data = {
+  detections: [
+    {
+      plate: "AP16CU6672",
+      camera: "CAM1",
+      time: "10:30 AM",
+      location: "Gate A",
+      lat: 16.506,
+      lng: 80.648
+    },
+    {
+      plate: "AP16CU6672",
+      camera: "CAM2",
+      time: "10:45 AM",
+      location: "Gate B",
+      lat: 16.507,
+      lng: 80.650
+    },
+    {
+      plate: "AP16CU6672",
+      camera: "CAM3",
+      time: "11:00 AM",
+      location: "Gate C",
+      lat: 16.508,
+      lng: 80.651
+    },
+    {
+      plate: "MH12AB1234",
+      camera: "CAM1",
+      time: "11:15 AM",
+      location: "Gate A",
+      lat: 16.509,
+      lng: 80.652
+    }
+  ]
+}
+
+export default data
